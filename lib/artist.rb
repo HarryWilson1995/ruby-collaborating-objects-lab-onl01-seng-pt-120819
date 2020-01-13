892951d0861adb51c2af19a319124ec2eed17073
Harry Wilson <harry.wilson1995a@gmail.com>
2020-01-13 17:35:05 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,7 +25,7 @@ class Artist
       if artist.name == name 
         self.name = name 
       else 
-        self.name = Artist.new 
+        self.name = Artist.new(name)
       end 
      end 
   end 
